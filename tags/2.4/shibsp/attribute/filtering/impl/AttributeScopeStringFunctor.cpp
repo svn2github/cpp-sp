503cddc60744abe0011f2fc02e1de0cafac39a9b
cantor <cantor@cb58f699-b61c-0410-a6fe-9272a202ed29>
2010-12-12 23:24:57 +0000

    Tagging 2.4 release.
    git-svn-id: https://svn.shibboleth.net/cpp-sp@3385 cb58f699-b61c-0410-a6fe-9272a202ed29

---
+++ tags/2.4/shibsp/attribute/filtering/impl/AttributeScopeStringFunctor.cpp
@@ -0,0 +1,122 @@
+/*
+ *  Copyright 2001-2010 Internet2
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * AttributeScopeStringFunctor.cpp
+ * 
+ * A match function that matches the scope of an attribute value against
+ * the specified value.
+ */
+
+#include "internal.h"
+#include "exceptions.h"
+#include "attribute/Attribute.h"
+#include "attribute/filtering/FilteringContext.h"
+#include "attribute/filtering/FilterPolicyContext.h"
+#include "attribute/filtering/MatchFunctor.h"
+
+#include <xmltooling/util/XMLHelper.h>
+
+using namespace shibsp;
+using namespace std;
+using xmltooling::XMLHelper;
+
+namespace shibsp {
+
+    static const XMLCh attributeID[] =  UNICODE_LITERAL_11(a,t,t,r,i,b,u,t,e,I,D);
+    static const XMLCh ignoreCase[] =   UNICODE_LITERAL_10(i,g,n,o,r,e,C,a,s,e);
+    static const XMLCh value[] =        UNICODE_LITERAL_5(v,a,l,u,e);
+
+    /**
+     * A match function that matches the scope of an attribute value against the specified value.
+     */
+    class SHIBSP_DLLLOCAL AttributeScopeStringFunctor : public MatchFunctor
+    {
+        string m_attributeID;
+        char* m_value;
+        bool m_ignoreCase;
+
+        bool hasScope(const FilteringContext& filterContext) const;
+
+    public:
+        AttributeScopeStringFunctor(const DOMElement* e)
+            : m_attributeID(XMLHelper::getAttrString(e, nullptr, attributeID)),
+                m_value(e ? xmltooling::toUTF8(e->getAttributeNS(nullptr,value)) : nullptr),
+                m_ignoreCase(XMLHelper::getAttrBool(e, false, ignoreCase)) {
+            if (!m_value || !*m_value) {
+                delete[] m_value;
+                throw ConfigurationException("AttributeScopeString MatchFunctor requires non-empty value attribute.");
+            }
+        }
+
+        virtual ~AttributeScopeStringFunctor() {
+            delete[] m_value;
+        }
+
+        bool evaluatePolicyRequirement(const FilteringContext& filterContext) const {
+            if (m_attributeID.empty())
+                throw AttributeFilteringException("No attributeID specified.");
+            return hasScope(filterContext);
+        }
+
+        bool evaluatePermitValue(const FilteringContext& filterContext, const Attribute& attribute, size_t index) const {
+            if (m_attributeID.empty() || m_attributeID == attribute.getId()) {
+                if (m_ignoreCase) {
+#ifdef HAVE_STRCASECMP
+                    return !strcasecmp(attribute.getScope(index), m_value);
+#else
+                    return !stricmp(attribute.getScope(index), m_value);
+#endif
+                }
+                else
+                    return !strcmp(attribute.getScope(index), m_value);
+            }
+            return hasScope(filterContext);
+        }
+    };
+
+    MatchFunctor* SHIBSP_DLLLOCAL AttributeScopeStringFactory(const std::pair<const FilterPolicyContext*,const DOMElement*>& p)
+    {
+        return new AttributeScopeStringFunctor(p.second);
+    }
+
+};
+
+bool AttributeScopeStringFunctor::hasScope(const FilteringContext& filterContext) const
+{
+    size_t count;
+    pair<multimap<string,Attribute*>::const_iterator,multimap<string,Attribute*>::const_iterator> attrs =
+        filterContext.getAttributes().equal_range(m_attributeID);
+    for (; attrs.first != attrs.second; ++attrs.first) {
+        count = attrs.first->second->valueCount();
+        for (size_t index = 0; index < count; ++index) {
+            if (m_ignoreCase) {
+#ifdef HAVE_STRCASECMP
+                if (!strcasecmp(attrs.first->second->getScope(index), m_value))
+                    return true;
+#else
+                if (!stricmp(attrs.first->second->getScope(index), m_value))
+                    return true;
+#endif
+            }
+            else {
+                if (!strcmp(attrs.first->second->getScope(index), m_value))
+                    return true;
+            }
+        }
+    }
+    return false;
+}
---
+++ tags/2.4/shibsp/util/DOMPropertySet.h
@@ -0,0 +1,89 @@
+/*
+ *  Copyright 2001-2010 Internet2
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file shibsp/util/DOMPropertySet.h
+ * 
+ * DOM-based property set implementation.
+ */
+
+#ifndef __shibsp_dompropset_h__
+#define __shibsp_dompropset_h__
+
+#include <shibsp/util/PropertySet.h>
+
+#include <xmltooling/logging.h>
+
+namespace shibsp {
+
+    /**
+     * DOM-based property set implementation.
+     */
+    class SHIBSP_API DOMPropertySet : public virtual PropertySet
+    {
+    public:
+        DOMPropertySet();
+        
+        virtual ~DOMPropertySet();
+
+        const PropertySet* getParent() const;
+        void setParent(const PropertySet* parent);
+        std::pair<bool,bool> getBool(const char* name, const char* ns=nullptr) const;
+        std::pair<bool,const char*> getString(const char* name, const char* ns=nullptr) const;
+        std::pair<bool,const XMLCh*> getXMLString(const char* name, const char* ns=nullptr) const;
+        std::pair<bool,unsigned int> getUnsignedInt(const char* name, const char* ns=nullptr) const;
+        std::pair<bool,int> getInt(const char* name, const char* ns=nullptr) const;
+        void getAll(std::map<std::string,const char*>& properties) const;
+        const PropertySet* getPropertySet(const char* name, const char* ns=shibspconstants::ASCII_SHIB2SPCONFIG_NS) const;
+        const xercesc::DOMElement* getElement() const;
+
+        /**
+         * Loads the property set from a DOM element.
+         * 
+         * @param e         root element of property set
+         * @param log       optional log object for tracing
+         * @param filter    optional filter controls what child elements to include as nested PropertySets
+         * @param remapper  optional map of property rename rules for legacy property support
+         */
+        void load(
+            const xercesc::DOMElement* e,
+            xmltooling::logging::Category* log=nullptr,
+            xercesc::DOMNodeFilter* filter=nullptr,
+            const std::map<std::string,std::string>* remapper=nullptr
+            );
+
+    protected:
+        /**
+         * Post-load injection of a property, for use by subclasses.
+         *
+         * @param name  property name
+         * @param val   property value
+         * @param ns    property namespace
+         * @return  true iff the property was successfully set
+         */
+        bool setProperty(const char* name, const char* val, const char* ns=nullptr);
+
+    private:
+        const PropertySet* m_parent;
+        const xercesc::DOMElement* m_root;
+        std::map<std::string,std::pair<char*,const XMLCh*> > m_map;
+        std::map<std::string,DOMPropertySet*> m_nested;
+        std::vector<xmltooling::xstring> m_injected;
+    };
+
+};
+
+#endif /* __shibsp_dompropset_h__ */
